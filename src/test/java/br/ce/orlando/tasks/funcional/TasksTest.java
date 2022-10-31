24f33c8d97b42f139ba1b190dfbe4eb3a756ce39
orlando-dev <orlando.junior07@hotmail.com>
2022-10-31 02:09:13 -0300

    IP local edit

--- src/test/java/br/ce/orlando/tasks/funcional/TasksTest.java
+++ src/test/java/br/ce/orlando/tasks/funcional/TasksTest.java
@@ -16,8 +16,8 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 //		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://172.26.185.43:4444/wd/hub"), cap);
-		driver.navigate().to("http://172.26.185.43:8001/tasks");
+		WebDriver driver = new RemoteWebDriver(new URL("http://198.168.18.80:4444/wd/hub"), cap);
+		driver.navigate().to("http://198.168.18.80:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
