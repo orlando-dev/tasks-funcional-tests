af04938a9ccf7a22e3c77d9554b4f35486cd6307
orlando-dev <orlando.junior07@hotmail.com>
2022-11-08 19:47:25 -0300

    Change ip

--- src/test/java/br/ce/orlando/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/ce/orlando/tasks/prod/HealthCheckIT.java
@@ -16,9 +16,9 @@ public class HealthCheckIT {
 	@Test
 	public void healthCheck() throws MalformedURLException {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.18.80:4444/wd/hub"), cap);
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.18.72:4444/wd/hub"), cap);
 		try {
-			driver.navigate().to("http://192.168.18.80:9999/tasks");
+			driver.navigate().to("http://192.168.18.72:9999/tasks");
 			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 			
 			String version = driver.findElement(By.id("version")).getText();
