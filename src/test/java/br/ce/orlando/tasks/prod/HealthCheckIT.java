6feaaaca7f1efdafd3ee805456a48be8624cb292
orlando-dev <orlando.junior07@hotmail.com>
2023-02-25 15:05:21 -0300

    Testes

--- src/test/java/br/ce/orlando/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/ce/orlando/tasks/prod/HealthCheckIT.java
@@ -12,18 +12,18 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class HealthCheckIT {
-	private String noteRemoteDriver = "http://172.26.185.107:4444/wd/hub";
-	private String noteNavigate = "http://172.26.185.107:9999/tasks";
+//	private String noteRemoteDriver = "http://172.26.185.107:4444/wd/hub";
+//	private String noteNavigate = "http://172.26.185.107:9999/tasks";
 	
-//	private String pcRemoteDriver = "http://192.168.18.80:4444/wd/hub";
-//	private String pcNavigate = "http://192.168.18.80:9999/tasks";
+	private String pcRemoteDriver = "http://192.168.18.80:4444/wd/hub";
+	private String pcNavigate = "http://192.168.18.80:9999/tasks";
 
 	@Test
 	public void healthCheck() throws MalformedURLException {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL(noteRemoteDriver), cap);
+		WebDriver driver = new RemoteWebDriver(new URL(pcRemoteDriver), cap);
 		try {
-			driver.navigate().to(noteNavigate);
+			driver.navigate().to(pcNavigate);
 			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 			
 			String version = driver.findElement(By.id("version")).getText();
